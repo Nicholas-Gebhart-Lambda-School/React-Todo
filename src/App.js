9e24092e0468d1a373b17ed07227d1931a71adf7
gebhartn <nicholas.gebhart@gmail.com>
2019-09-30 14:15:45 -0700

    Added skeleton
    Add feature is working, but not clearing inputs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React from "react";
+import ItemTable from "./components/Tables/ItemTable";
+import AddItemForm from "./components/Forms/AddItemForm";
+
+const listData = [
+  { id: 1, what: "Implement React Router", when: "1:00PM" },
+  { id: 2, what: "Wrap App with BrowserRouter", when: "1:30PM" },
+  { id: 3, what: "Initialize Routes in App.js", when: "1:35PM" }
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = { list: listData };
+  }
+
+  addItem = itemName => {
+    const item = {
+      id: Date.now(),
+      what: itemName.what,
+      when: itemName.when
+    };
+    this.setState({
+      list: [...this.state.list, item]
+    });
+  };
+
   render() {
+    console.log(this.state);
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="container">
+        <h1>To-Do List</h1>
+        <div className="flex-row">
+          <div className="flex-large">
+            <h2>Add item</h2>
+            <AddItemForm addItem={this.addItem} />
+          </div>
+          <div className="flex-large">
+            <h2>View items</h2>
+            <ItemTable list={this.state.list} />
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/Forms/AddItemForm.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+class AddItemForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      id: null,
+      what: "",
+      when: ""
+    };
+  }
+
+  handleInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          if (!this.state.what || !this.state.when) return;
+
+          console.log(this.state);
+          this.props.addItem(this.state);
+          this.setState({
+            id: null,
+            what: "",
+            when: ""
+          });
+        }}
+      >
+        <label>What</label>
+        <input
+          type="text"
+          name="what"
+          value={this.what}
+          onChange={this.handleInput}
+        />
+        <label>When</label>
+        <input
+          type="text"
+          name="when"
+          value={this.when}
+          onChange={this.handleInput}
+        />
+        <button>Add new item</button>
+      </form>
+    );
+  }
+}
+
+export default AddItemForm;
